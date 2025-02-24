57658a7a51b7e7cff19fa4d363b37335475febbc
Adeline A. Ackun <adelineackun@gmail.com>
2025-02-24 09:29:20 +0000

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import TopBar from "./components/topbar";
+import TopBar from "./components/Topbar";
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
